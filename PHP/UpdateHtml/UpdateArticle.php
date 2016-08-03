f8d83d94300fb997d66860177374564f5e3d4cab
ztyzly00 <ztyzly00@126.com>
2016-08-03 17:27:04 +0800

    backup

--- PHP/UpdateHtml/UpdateArticle.php
+++ PHP/UpdateHtml/UpdateArticle.php
@@ -14,29 +14,29 @@ $array = UpdateColumn::$column_array;
 $redis = new redis();
 $redis->connect('127.0.0.1', 6379);
 
-//for ($i = 0; $i < count($array); $i++) {
-//    $list = NewsList::getNewsListByColumnId($array[$i], 0, 20);
-//    echo count($list);
-//    for ($j = 0; $j < count($list); $j++) {
-//        echo 1;
-//        $url = "http://xm.ntwifi.cn/m.ntjoy.com/Page/news.php?id=" . $list[$j]['id'];
-//        $rr = file_get_contents($url);
-//    }
-//}
-
-$mh = curl_multi_init();
-
 for ($i = 0; $i < count($array); $i++) {
     $list = NewsList::getNewsListByColumnId($array[$i], 0, 20);
     for ($j = 0; $j < count($list); $j++) {
         $url = "http://xm.ntwifi.cn/m.ntjoy.com/Page/news.php?id=" . $list[$j]['id'];
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_multi_add_handle($mh, $ch);
+        print_r($url . "\n");
+        $rr = file_get_contents($url);
     }
 }
 
-$running = null;
-do {
-    curl_multi_exec($mh, $running);
-} while ($running > 0);
+//不推荐用，会造成单核100% ，卡死大部分正常请求
+//$mh = curl_multi_init();
+//
+//for ($i = 0; $i < count($array); $i++) {
+//    $list = NewsList::getNewsListByColumnId($array[$i], 0, 20);
+//    for ($j = 0; $j < count($list); $j++) {
+//        $url = "http://xm.ntwifi.cn/m.ntjoy.com/Page/news.php?id=" . $list[$j]['id'];
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, $url);
+//        curl_multi_add_handle($mh, $ch);
+//    }
+//}
+//
+//$running = null;
+//do {
+//    curl_multi_exec($mh, $running);
+//} while ($running > 0);
